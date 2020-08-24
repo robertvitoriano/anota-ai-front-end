ad58179b2a880869ac9387d75439ee2b0ff80e9a
robertvitoriano <robertvitorianodev@gmail.com>
2020-08-24 15:00:29 -0300

    next and previous buttons created

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -5,10 +5,16 @@ import api from '../../services/api'
 import Header from './../../components/Header'
 import arrow from './arrow.png'
 
+
 const Home = ({ match }) => {
     const [notes, setNotes] = useState([]);
+    const[initialNote,setInitialNote] = useState(0);
+    const [lastNote, setLastNote] = useState(7);
 
-    let history = useHistory();
+    
+
+
+    const history = useHistory();
 
     useEffect(() => {
         async function loadNotes() {
@@ -31,39 +37,55 @@ const Home = ({ match }) => {
 
     }
 
+    function handleNextNotesButton() {
 
 
-    return (<div className="home-container" >
+
+    }
+
+
+    return (
+      <div className="home-container">
         <Header match={match}></Header>
 
+        <div className="home-content">
+          <div className="next-button">
+            <img className="arrow-icon" src={arrow} />
+          </div>
+          <div className="notes-container">
+            {notes.length > 0 ? (
 
-
-
-        <div className="next-button">
-            <img src={arrow} />
+              <ul className="notes-list">
+                {notes.map((note,index) => (
+                    
+                  <a
+                    key={note._id}
+                    onClick={() => {
+                      handleUpdate(note._id);
+                    }}
+                    href="#"
+                  >
+                    <li className="note-container">
+                      <div className="note-title">
+                        <h2>{note.title}</h2>
+                      </div>
+                      <div className="note-body-list">
+                        <p>{note.body}</p>
+                      </div>
+                    </li>
+                  </a>
+                ))}
+              </ul>
+            ) : (
+              <h1>Nenhuma anotação ainda</h1>
+            )}
+          </div>
+          <div className="previous-button">
+            <img className="arrow-icon previous-icon" src={arrow} />
+          </div>
         </div>
-        {notes.length > 0 ? (
-            <ul className="notes-list">
-                {notes.map((note) =>
-                    <a key={note._id} onClick={() => { handleUpdate(note._id) }} href="#">
-                        <li className="note-container">
-                            <div className="note-title">
-                                <h2>{note.title}</h2>
-                            </div>
-                            <div className="note-body-list">
-                                <p>{note.body}</p>
-                            </div>
-                        </li>
-                    </a>
-
-                )}
-            </ul>
-
-        ) : <h1>Nenhuma anotação ainda</h1>}
-
-
-
-    </div>)
+      </div>
+    );
 }
 
 
