bb8b463b6d5f1a948f4cc91fea3f199553ee6570
robertvitoriano <robertvitorianodev@gmail.com>
2020-08-25 19:53:17 -0300

    Footer added

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,64 +1,73 @@
-import React, { useState } from 'react'
-
+import React, { useState } from "react";
+import Footer from "./../../components/Footer";
 // import { Link } from 'react-router-dom';
-import api from '../../services/api'
+import api from "../../services/api";
 
-import './login.css'
-const Login = ({history})=>{
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    
-    async function handleSubmit(event) {
-        event.preventDefault()
-        const response = await api.post('/users/login', {
-            email: email,
-            password:password
-        })
-           
-     
-       // response será o user encontrado no banco de dados
-        const {token,user } = response.data
-        localStorage.setItem("Authorization", token);
+import "./login.css";
+const Login = ({ history }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-        history.push(`/user/${user._id}`)
-       
-    }
-    function handleSignInButton(event){
-        event.preventDefault()
-        history.push(`/user/signup`)
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const response = await api.post("/users/login", {
+      email: email,
+      password: password,
+    });
 
-    }
+    // response será o user encontrado no banco de dados
+    const { token, user } = response.data;
+    localStorage.setItem("Authorization", token);
 
+    history.push(`/user/${user._id}`);
+  }
+  function handleSignInButton(event) {
+    event.preventDefault();
+    history.push(`/user/signup`);
+  }
 
-    return(<div className="login-container">
-            <h1 className="welcome-message">Seja Bem-vindo(a)</h1>
-        <div className="form-container">
-            <form className="login-form" onSubmit={handleSubmit}>
-                <input className="email-input" 
-                placeholder="Digite seu email" 
-                name="email"
-                 value={email}
-                 onChange={e=> setEmail(e.target.value)}
-                 ></input>
-                <input className="password-input" 
-                type="password"
-                 placeholder="Digite sua senha" 
-                 name="password" 
-                 value={password}
-                 onChange={e=>setPassword(e.target.value)}
-                 ></input>
-             
-                <button type="submit" className="login-button button">Login</button>
-            </form>
-            </div>
-        <h2 className="signup-message">Ainda não é cadastrado ? Clique no botão abaixo e junte-sea nós!</h2>
-        <button type="button" onClick={e => handleSignInButton(e)} className="signup-button button">Sign Up</button>
-        <h2 className="signup-message"><strong>Feito por Robert da Silva Vitoriano</strong></h2>
+  return (
+    <div className="login-container">
+      <h1 className="welcome-message">Seja Bem-vindo(a)</h1>
+      <div className="form-container">
+        <form className="login-form" onSubmit={handleSubmit}>
+          <input
+            className="email-input"
+            placeholder="Digite seu email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          ></input>
+          <input
+            className="password-input"
+            type="password"
+            placeholder="Digite sua senha"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          ></input>
 
-
-    </div>)
-}
-
-
+          <button type="submit" className="login-button button">
+            Login
+          </button>
+        </form>
+      </div>
+      <h2 className="signup-message">
+        Ainda não é cadastrado ? Clique no botão abaixo e junte-sea nós!
+      </h2>
+      <button
+        type="button"
+        onClick={(e) => handleSignInButton(e)}
+        className="signup-button button"
+      >
+        Sign Up
+      </button>
+      <h2 className="signup-message">
+        <strong>Feito por Robert da Silva Vitoriano</strong>
+      </h2>
+      <Footer></Footer>
+    </div>
+  );
+};
 
 export default Login;
