934c4aa2cfee1d1a2afdb2c5bba5ef2238723d91
robertvitoriano <robertvitorianodev@gmail.com>
2020-09-12 15:12:59 -0300

    categories being loaded

--- src/pages/Categories/index.js
+++ src/pages/Categories/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import api from './../../services/api'
+import api from "./../../services/api";
 
 import Header from "./../../components/Header";
 import Footer from "./../../components/Footer";
@@ -7,27 +7,32 @@ import "./index.css";
 function Categories({ match }) {
   const [categories, setCategories] = useState([]);
 
-  useEffect(()=>{
-      async function loadCategories(){
-          const response = await api.get("/" + match.params.userId + "/categories",
-            {
-              headers: {
-                userauth: localStorage.getItem("Authorization"),
-              },
-            }
-          );
-          setCategories(response.data);
-          console.log(response);
-      }
-      loadCategories()
- 
-  })
-  console.log(categories)
+  useEffect(() => {
+    async function loadCategories() {
+      const response = await api.get(
+        "/" + match.params.userId + "/categories",
+        {
+          headers: {
+            userauth: localStorage.getItem("Authorization"),
+          },
+        }
+      );
+      setCategories(response.data);
+      console.log(response);
+    }
+    loadCategories();
+  });
+  console.log(categories);
 
   return (
     <div className="categories-container">
       <Header match={match}></Header>
       <div className="categories-content">
+        <div className=" categories-buttons categories-buttons-left">
+          <a className="header-button categories-button">Criar Anotação</a>
+          <a className="header-button categories-button">Deletar</a>
+          <a className="header-button categories-button"></a>
+        </div>
         <div className="categories-scroll-list">
           <ul>
             <div className="categories-list">
@@ -36,6 +41,11 @@ function Categories({ match }) {
               ))}
             </div>
           </ul>
+        </div>
+        <div className=" categories-buttons categories-buttons-right">
+          <a className="header-button categories-button"></a>
+          <a className="header-button categories-button"></a>
+          <a className="header-button categories-button"></a>
         </div>
       </div>
       <Footer></Footer>
