b01490b05e4fbbda86dd156f619a9eb12c6ee496
robertvitoriano <robertvitorianodev@gmail.com>
2020-11-01 17:49:12 -0300

    setting add to category route

--- src/pages/AddToCategory/styled.js
+++ src/pages/AddToCategory/styled.js
@@ -1,11 +1,11 @@
 import styled from "styled-components";
-import image from "./typewriter.jpg";
+// import image from "./typewriter.jpg";
 
 
 export const Wrapper= styled.div`
   width: 100vw;
   height: 100vh;
-  background-image: url(${image});
+  /* background-image: url(${image}); */
   background-size: cover;
   background-repeat: no-repeat;
   display: flex;
