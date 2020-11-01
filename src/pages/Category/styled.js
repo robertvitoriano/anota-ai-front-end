2ff10ef7680971993bee6ba6d29c07e1924c6e12
robertvitoriano <robertvitorianodev@gmail.com>
2020-11-01 15:38:14 -0300

    add create category modal

--- src/pages/Category/styled.js
+++ src/pages/Category/styled.js
@@ -7,8 +7,19 @@ export const Wrapper = styled.div`
   background-image: url(${image});
   background-size: cover;
   background-repeat: no-repeat;
-  display:flex;
-  justify-content:center;
+  display: flex;
+  justify-content: center;
+
+  div::-webkit-scrollbar {
+    width: 1em;
+  }
+  div::-webkit-scrollbar-track {
+    -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+  }
+  div::-webkit-scrollbar-thumb {
+    background-color: #817059;
+    border-radius: 10px;
+  }
 `;
 
 export const NotesWrapper = styled.div`
@@ -18,12 +29,23 @@ export const NotesWrapper = styled.div`
   grid-template-columns: 20vw 20vw 20vw;
   grid-gap: 8%;
   position: absolute;
-  position:relative;
-  right:20px;
-  bottom:30px;
-  height:600px;
-  width:1400px;
-  overflow-y:scroll;
+  position: relative;
+  right: 20px;
+  bottom: 30px;
+  height: 600px;
+  width: 1400px;
+  overflow-y: scroll;
+
+  -webkit-scrollbar {
+    width: 1em;
+  }
+  -webkit-scrollbar-track {
+    -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+  }
+  -webkit-scrollbar-thumb {
+    background-color: #817059;
+    border-radius: 10px;
+  }
 `;
 
 export const Title = styled.div`
